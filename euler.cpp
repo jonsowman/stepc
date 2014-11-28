bf2bd5c484f300fee9deca5046d36923d6ae67b1
Jon Sowman <jon@jonsowman.com>
2014-11-28 11:39:26 +0000

    Moved LinSystem and Simulator into their own files. Simulator::simulate() can now simulate input from a controller

--- euler.cpp
+++ euler.cpp
@@ -2,9 +2,10 @@
  * Fast linear system simulator using Boost
  * and uBLAS.
  *
- * Jon Sowman 2014 <jon@jonsowman.com>
- *
- * All Rights Reserved
+ * @file euler.cpp
+ * @author Jon Sowman, University of Southampton <j.sowman@soton.ac.uk>
+ * @copyright Jon Sowman 2014, All Rights Reserved
+ * @addtogroup stepc
  */
 
 #include <iostream>
@@ -14,66 +15,11 @@
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
 
+// Project local
+#include "simulator.hpp"
+#include "linsystem.hpp"
+
 namespace blas = boost::numeric::ublas;
-
-// Class prototypes
-class Simulator;
-class LinSystem;
-
-class Simulator
-{
-    public:
-
-    void setTimestep(double Ts)
-    {
-        if(Ts != 0.0)
-            _ts = Ts;
-    }
-
-    void setEndTime(double EndTime)
-    {
-        if(EndTime != 0.0)
-            _endtime = EndTime;
-    }
-
-    blas::vector<double> simulate(LinSystem sys, blas::vector<double> x);
-
-    private:
-        double _ts;
-        double _endtime;
-};
-
-class LinSystem
-{
-    public:
-        LinSystem(const int order)
-        {
-            A.resize(order, order);
-        }
-
-        blas::matrix<double> A;
-};
-
-blas::vector<double> Simulator::simulate(LinSystem sys, blas::vector<double> x)
-{
-    // Sanity check simulation parameters
-    if(_ts == 0.0 || _endtime == 0.0 || _ts > _endtime)
-    {
-        std::cout << "Sim params don't make sense!" << std::endl;
-        return x;
-    }
-
-    // Step in time
-    for(double i = 0.0; i<=_endtime; i+=_ts)
-    {
-        // xdot = A*x
-        blas::vector<double> xdot = prod(sys.A, x);
-
-        // Integrate using forwards Euler (could do better here)
-        x = x + _ts * xdot;
-    }
-    return x;
-}
 
 int main(void)
 {
@@ -83,12 +29,22 @@ int main(void)
     x(0) = 1.0;
     x(1) = 0.0;
 
-    // Create linear system of order 2
-    LinSystem sys(2);
+    // Create linear system of order 2 and 1 input and 1 output
+    LinSystem sys(2, 1, 1);
+
+    // Define A (2x2)
     sys.A(0,0) = 0;
     sys.A(0,1) = 1;
     sys.A(1,0) = -1;
     sys.A(1,1) = 0;
+
+    // Define B (2x1)
+    sys.B(0,0) = 0;
+    sys.B(1,0) = -1;
+
+    // Define C (1x2)
+    sys.C(0,0) = 1;
+    sys.C(0,1) = 0;
 
     // Create a fixed timestep simulator
     Simulator Sim;
