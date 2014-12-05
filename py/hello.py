5216a9dfa0eb25651e62d6e1f2a45e9e14688be9
Jon Sowman <jon@jonsowman.com>
2014-12-05 14:02:03 +0000

    Python version

---
+++ py/hello.py
@@ -0,0 +1,7 @@
+# STEPC
+# A fast and extensible linear system and controller simulator
+# for evaluation of predictive controllers
+#
+# Jon Sowman 2014 <j.sowman@soton.ac.uk>
+
+print "Hello, world!"
